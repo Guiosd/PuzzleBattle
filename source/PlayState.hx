4f6f6a10b1918a5671a1e38d7ce504bdda53ca9d
Guiosd <53371875+Guiosd@users.noreply.github.com>
2021-12-12 01:11:36 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,18 +1,135 @@
 package;
 
-import flixel.text.FlxText;
+#if desktop
+import Discord.DiscordClient;
+#end
+import Section.SwagSection;
+import Song.SwagSong;
+import WiggleEffect.WiggleEffectType;
+import flixel.FlxBasic;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxObject;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.FlxSubState;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.effects.FlxTrail;
+import flixel.addons.effects.FlxTrailArea;
+import flixel.addons.effects.chainable.FlxEffectSprite;
+import flixel.addons.effects.chainable.FlxWaveEffect;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.graphics.atlas.FlxAtlas;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.system.FlxSound;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.ui.FlxBar;
+import flixel.util.FlxCollision;
+import flixel.util.FlxColor;
+import flixel.util.FlxSort;
+import flixel.util.FlxStringUtil;
+import flixel.util.FlxTimer;
+import haxe.Json;
+import lime.utils.Assets;
+import openfl.display.BlendMode;
+import openfl.display.StageQuality;
+import openfl.filters.ShaderFilter;
 
-class PlayState extends FlxState
+using StringTools;
+
+class PlayState extends MusicBeatState
 {
-	override public function create()
-	{
-		super.create();
-		add(new FlxText("Hello World", 32).screenCenter());
-	}
+	public static var curStage:String = '';
+	public static var SONG:SwagSong;
+	public static var isStoryMode:Bool = false;
+	public static var storyWeek:Int = 0;
+	public static var storyPlaylist:Array<String> = [];
+	public static var storyDifficulty:Int = 1;
 
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-	}
-}
+	var halloweenLevel:Bool = false;
+
+	private var vocals:FlxSound;
+
+	private var mr:Character;
+	private var lila:Character;
+	private var puzzler:puzzler;
+
+	private var notes:FlxTypedGroup<Note>;
+	private var unspawnNotes:Array<Note> = [];
+
+	private var strumLine:FlxSprite;
+	private var curSection:Int = 0;
+
+	private var camFollow:FlxObject;
+
+	private static var prevCamFollow:FlxObject;
+
+	private var strumLineNotes:FlxTypedGroup<FlxSprite>;
+	private var playerStrums:FlxTypedGroup<FlxSprite>;
+
+	private var camZooming:Bool = false;
+	private var curSong:String = "";
+
+	private var LilaSpeed:Int = 1;
+	private var health:Float = 1;
+	private var combo:Int = 0;
+
+	private var healthBarBG:FlxSprite;
+	private var healthBar:FlxBar;
+
+	private var generatedMusic:Bool = false;
+	private var startingSong:Bool = false;
+
+	private var iconP1:HealthIcon;
+	private var iconP2:HealthIcon;
+	private var camHUD:FlxCamera;
+	private var camGame:FlxCamera;
+
+	var dialogue:Array<String> = ['blah blah blah', 'coolswag'];
+
+	var halloweenBG:FlxSprite;
+	var isHalloween:Bool = false;
+
+	var LACityLights:FlxTypedGroup<FlxSprite>;
+	var LATrain:FlxSprite;
+	var trainSound:FlxSound;
+
+	var limo:FlxSprite;
+	var grpLimoDancers:FlxTypedGroup<BackgroundDancer>;
+	var fastCar:FlxSprite;
+
+	var upperBoppers:FlxSprite;
+	var bottomBoppers:FlxSprite;
+	var santa:FlxSprite;
+
+	var bgGirls:BackgroundGirls;
+	var wiggleStuff:WiggleEffect = new WiggleEffect();
+
+	var talking:Bool = true;
+	var songScore:Int = 0;
+	var scoreTxt:FlxText;
+
+	public static var campaignScore:Int = 0;
+
+	var defaultCamZoom:Float = 1.05;
+
+	// how big to stretch the pixel art assets
+	public static var daPixelZoom:Float = 6;
+
+	var inCutscene:Bool = false;
+
+	#if desktop
+	// Discord RPC variables
+	var storyDifficultyText:String = "";
+	var iconRPC:String = "";
+	var songLength:Float = 0;
+	var detailsText:String = "";
+	var detailsPausedText:String = "";
+	#end
